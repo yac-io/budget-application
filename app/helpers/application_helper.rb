0e58607b5411a09ba6fb85673109caf0242c9613
Geoffroy Weyer <webweyer@gmail.com>
2017-12-04 21:22:02 -0800

    basic style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  # Check if a particular controller is the current one
+  #
+  # args - One or more controller names to check
+  #
+  # Examples
+  #
+  #   # On TreeController
+  #   current_controller?(:tree)           # => true
+  #   current_controller?(:commits)        # => false
+  #   current_controller?(:commits, :tree) # => true
+  def current_controller?(*args)
+    args.any? do |v|
+      v.to_s.downcase == controller.controller_name || v.to_s.downcase == controller.controller_path
+    end
+  end
+
 end
