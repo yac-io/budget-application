a9522fd4b7e803ee032e653f2ba757f64955a479
Geoffroy Weyer <webweyer@gmail.com>
2017-12-26 18:14:14 -0800

    Select dropdown for paiment mean account

--- spec/views/payment_means/edit.html.erb_spec.rb
+++ spec/views/payment_means/edit.html.erb_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "payment_means/edit", type: :view do
+RSpec.describe 'payment_means/edit', type: :view do
   before(:each) do
+    allow(view).to receive(:current_user).and_return(create(:user))
     @payment_mean = assign(:payment_mean, create(:payment_mean))
   end
 
-  it "renders the edit payment_mean form" do
+  it 'renders the edit payment_mean form' do
     render
 
-    assert_select "form[action=?][method=?]", payment_mean_path(@payment_mean), "post" do
+    assert_select 'form[action=?][method=?]', payment_mean_path(@payment_mean), 'post' do
 
-      assert_select "input[name=?]", "payment_mean[name]"
+      assert_select 'input[name=?]', 'payment_mean[name]'
 
-      assert_select "input[name=?]", "payment_mean[currency]"
+      assert_select 'input[name=?]', 'payment_mean[currency]'
 
-      assert_select "input[name=?]", "payment_mean[account_id]"
+      assert_select 'select[name=?]', 'payment_mean[account_id]'
     end
   end
 end
--- spec/views/payment_means/new.html.erb_spec.rb
+++ spec/views/payment_means/new.html.erb_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "payment_means/new", type: :view do
+RSpec.describe 'payment_means/new', type: :view do
   before(:each) do
+    allow(view).to receive(:current_user).and_return(create(:user))
     assign(:payment_mean, PaymentMean.new(
-      :name => "MyString",
-      :currency => "MyString",
+      :name => 'MyString',
+      :currency => 'MyString',
       :account => nil
     ))
   end
 
-  it "renders new payment_mean form" do
+  it 'renders new payment_mean form' do
     render
 
-    assert_select "form[action=?][method=?]", payment_means_path, "post" do
+    assert_select 'form[action=?][method=?]', payment_means_path, 'post' do
 
-      assert_select "input[name=?]", "payment_mean[name]"
+      assert_select 'input[name=?]', 'payment_mean[name]'
 
-      assert_select "input[name=?]", "payment_mean[currency]"
+      assert_select 'input[name=?]', 'payment_mean[currency]'
 
-      assert_select "input[name=?]", "payment_mean[account_id]"
+      assert_select 'select[name=?]', 'payment_mean[account_id]'
     end
   end
 end
