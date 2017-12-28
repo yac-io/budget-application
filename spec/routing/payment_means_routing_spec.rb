63bc44ca5aa1e37174310152735f32e5c78c61ed
Geoffroy Weyer <webweyer@gmail.com>
2017-12-27 20:24:48 -0800

    WIP #3, #4

--- spec/routing/payment_means_routing_spec.rb
+++ spec/routing/payment_means_routing_spec.rb
@@ -1,38 +1,38 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe PaymentMeansController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(:get => "/payment_means").to route_to("payment_means#index")
+    it 'routes to #index' do
+      expect(:get => '/payment-means').to route_to('payment_means#index')
     end
 
-    it "routes to #new" do
-      expect(:get => "/payment_means/new").to route_to("payment_means#new")
+    it 'routes to #new' do
+      expect(:get => '/payment-means/new').to route_to('payment_means#new')
     end
 
-    it "routes to #show" do
-      expect(:get => "/payment_means/1").to route_to("payment_means#show", :id => "1")
+    it 'routes to #show' do
+      expect(:get => '/payment-means/1').to route_to('payment_means#show', :id => '1')
     end
 
-    it "routes to #edit" do
-      expect(:get => "/payment_means/1/edit").to route_to("payment_means#edit", :id => "1")
+    it 'routes to #edit' do
+      expect(:get => '/payment-means/1/edit').to route_to('payment_means#edit', :id => '1')
     end
 
-    it "routes to #create" do
-      expect(:post => "/payment_means").to route_to("payment_means#create")
+    it 'routes to #create' do
+      expect(:post => '/payment-means').to route_to('payment_means#create')
     end
 
-    it "routes to #update via PUT" do
-      expect(:put => "/payment_means/1").to route_to("payment_means#update", :id => "1")
+    it 'routes to #update via PUT' do
+      expect(:put => '/payment-means/1').to route_to('payment_means#update', :id => '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(:patch => "/payment_means/1").to route_to("payment_means#update", :id => "1")
+    it 'routes to #update via PATCH' do
+      expect(:patch => '/payment-means/1').to route_to('payment_means#update', :id => '1')
     end
 
-    it "routes to #destroy" do
-      expect(:delete => "/payment_means/1").to route_to("payment_means#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(:delete => '/payment-means/1').to route_to('payment_means#destroy', :id => '1')
     end
 
   end
--- spec/views/payment_means/show.html.erb_spec.rb
+++ spec/views/payment_means/show.html.erb_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'payment_means/show', type: :view do
   before(:each) do
     account = create(:account, name: 'account name')
     @payment_mean = assign(:payment_mean, create(:payment_mean, name: 'Name', currency: 'USD', account: account))
+    @transactions = assign(:transactions, Transaction.all)
   end
 
   it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Name/)
     expect(rendered).to match(/USD/)
-    expect(rendered).to match(/account name/)
   end
 end
